0860c8483f95c3e197ef64e2e37734de6be8de13
furkanreyhan <mecmechul@gmail.com>
2024-04-19 15:59:09 +0300

    load balancer added

---
+++ student-service/src/main/java/com/furkanreyhan/studentservice/feignclients/AdrSerLoadBalancer.java
@@ -0,0 +1,15 @@
+package com.furkanreyhan.studentservice.feignclients;
+
+import feign.Feign;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+
+@LoadBalancerClient(value = "address-service")
+public class AdrSerLoadBalancer {
+    @LoadBalanced
+    @Bean
+    public Feign.Builder feignBuilder(){
+        return Feign.builder();
+    }
+}
